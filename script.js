aea9b4a5e67c96285a0ca2877702b2e11843aa65
md.jewel mia <md.jewelmia52@gmail.com>
2024-04-17 07:14:09 +0600

    added modal

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ document.addEventListener("DOMContentLoaded", function() {
         responsiveIcon.classList.toggle("collapsed"); // Toggle collapsed class on the button itself
     });
 });
+
+
+// Modal script
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+let currentPercentage = 50; // Initial percentage
+
+function togglePercentage() {
+  currentPercentage = 50; // Reset percentage to 50% when clicking on it
+  updateButtonText();
+}
+
+function updateButtonText() {
+  currentPercentage = Math.max(0, Math.min(100, currentPercentage)); // Ensure percentage stays within range
+  document.getElementById("percentage-btn").innerText = currentPercentage + "%";
+}
+
